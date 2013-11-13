aa69b375aa1174871c0a7cb5f9f10c9442688be4
Nada Amin <namin@alum.mit.edu>
2013-11-13 23:27:28 +0100

    stub for translating examples to core.typed

---
+++ src/grk2clj/examples.clj
@@ -0,0 +1,17 @@
+(ns grk2clj.examples
+  (:refer-clojure :exclude [< not=])
+  (:import (clojure.lang Seqable PersistentHashSet Symbol)
+           (java.io File))
+  (:require [clojure.core.typed :refer [ann inst cf fn> pfn> check-ns ann-form]]
+            [clojure.repl :refer [pst]]
+            [clojure.tools.analyzer :refer [ast]]))
+
+;;; Some examples from the paper
+;;; translated from Typed Racket to Typed Clojure
+;;; To type-check, load namespace in REPL execute (check-ns).
+
+;;; Example 2
+(ann f [(U String Number) -> Number])
+(defn f [x]
+  (if (number? x) (inc x) (count x)))
+
