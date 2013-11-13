9c82067030ac41cf373f10202b8cfd8603db7ddc
Nada Amin <namin@alum.mit.edu>
2013-11-13 23:16:15 +0100

    init

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject grk2clj "0.1.1-SNAPSHOT"
+  :description "From Greek to Clojure at Clojure/conj 2013"
+  :url "https://github.com/namin/grk2clj"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.logic "0.8.4"]
+                 [org.clojure/core.match "0.2.0"]
+                 [org.clojure/core.typed "0.2.19"]
+                 ])
